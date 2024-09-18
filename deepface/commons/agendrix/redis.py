d0e643ffd586ce4fdcdefbf681541b3e8cdb41f5
Philippe Poulin <p.poulin@agendrix.com>
2024-09-18 12:07:10 -0400

    Simplify Redis

--- deepface/commons/agendrix/redis.py
+++ deepface/commons/agendrix/redis.py
@@ -7,10 +7,4 @@ def initialize_redis():
     return redis.Redis(
         host=environ.get("REDIS_HOST", "localhost"),
         port=int(environ.get("REDIS_PORT", 0)),
-        username=environ.get("REDIS_USERNAME", None),
-        password=environ.get("REDIS_PASSWORD", None),
-        ssl=True,
-        ssl_certfile=environ.get("REDIS_SSL_CERTFILE", None),
-        ssl_keyfile=environ.get("REDIS_SSL_KEYFILE", None),
-        ssl_ca_certs=environ.get("REDIS_SSL_CA_CERTS", None),
     )
